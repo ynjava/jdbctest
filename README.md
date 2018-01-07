39e15c5db30056556cc76b30de0023308e62a2fa
tekintian <tekintian@gmail.com>
2018-01-07 19:22:13 +0800

    增加数据库文件，更新说明文档

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # jdbctest
 基于Servlet、JSP、JDBC、MySQL的一个简单的用户注册模块 java jdbctest  JDK1.8 JAVA数据库连接学习项目， Java jdbc demo , just for java jdbc learning
+
+
+JAVA学习交流QQ群:109857332
+
+
+安装视频
+https://v.youku.com/v_show/id_XMzI5ODczNDM2NA==.html
---
+++ sources/mydb.sql
@@ -0,0 +1,28 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : MariaDB-10.2_3306
+Source Server Version : 50505
+Source Host           : localhost:3306
+Source Database       : mydb
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2018-01-07 18:40:54
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for userinfo
+-- ----------------------------
+DROP TABLE IF EXISTS `userinfo`;
+CREATE TABLE `userinfo` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) DEFAULT NULL,
+  `realname` varchar(30) DEFAULT NULL,
+  `pswd` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
